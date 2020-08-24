6cc44234ebc851931d3a1f74efde29a382aa0f76
trofimov_d_a <derofim@y.ru>
2020-08-24 16:08:34 +0300

    refactor

--- cmake/Findsquarets.cmake
+++ cmake/Findsquarets.cmake
@@ -20,18 +20,22 @@ set(squarets_LIB CONAN_PKG::squarets)
 set(squarets_HEADER_DIR
   ${CONAN_SQUARETS_ROOT}/include
 )
-if(squarets_LOCAL_BUILD)
+# used by https://docs.conan.io/en/latest/developing_packages/workspaces.html
+if(TARGET squarets)
   # name of created target
   set(squarets_LIB squarets)
   # no '/include' dir on local build
   set(squarets_HEADER_DIR
     ${CONAN_SQUARETS_ROOT}
   )
-else()
+endif()
+
+if(EXISTS "${CMAKE_CURRENT_LIST_DIR}/cmake/squarets-config.cmake")
   # uses Config.cmake or a -config.cmake file
   # see https://gitlab.kitware.com/cmake/community/wikis/doc/tutorials/How-to-create-a-ProjectConfig.cmake-file
   # BELOW MUST BE EQUAL TO find_package(... CONFIG REQUIRED)
   # NOTE: find_package(CONFIG) not supported with EMSCRIPTEN, so use include()
   include(${CMAKE_CURRENT_LIST_DIR}/cmake/squarets-config.cmake)
-endif(squarets_LOCAL_BUILD)
+endif()
+
 message(STATUS "squarets_HEADER_DIR=${squarets_HEADER_DIR}")
